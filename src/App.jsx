be5a44710144af155d889355ff9330d60d91b875
Hubert369 <86267581+hubertbrito@users.noreply.github.com>
2024-01-25 15:07:51 +0000

    Criação da pasta components/Card/Card.jsx e refatorar o código do app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 
 import './App.css'
+import Card from './components/Cards/Card'
 
 function App() {
   return (
     <>
-      <div className='cards'>
-        <h2>Rick Sanchez</h2>
-        <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'/>
-      </div>
-      <div className='cards'>
-        <h2>Morthy</h2>
-        <img src='https://rickandmortyapi.com/api/character/avatar/2.jpeg'/>
-      </div>
+     <Card />
+     <Card />
     </>
   )
 }
---
+++ src/components/Cards/Card.jsx
@@ -0,0 +1,13 @@
+function Card (){
+    return (
+        <>
+          <div className='cards'>
+            <h2>Rick Sanchez</h2>
+            <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'/>
+          </div>
+        </>
+      )
+}
+
+
+export default Card
