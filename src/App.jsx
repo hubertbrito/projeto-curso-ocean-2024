beaf87da1ca291c178f52861ef5c7dff4f35829c
Hubert369 <86267581+hubertbrito@users.noreply.github.com>
2024-01-25 14:42:41 +0000

    Estilização dos cards e imgs

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
+      <div className='cards'>
         <h2>Rick Sanchez</h2>
         <img src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'/>
+      </div>
+      <div className='cards'>
+        <h2>Morthy</h2>
+        <img src='https://rickandmortyapi.com/api/character/avatar/2.jpeg'/>
       </div>
     </>
   )
